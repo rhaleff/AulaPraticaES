7eb9a636aa9fbb67237635f9e2dd4909ae03bf85
bruno.oliveira <bruno.oliveira@LAB5-DCX-26.dcx.ufpb.br>
2016-06-07 11:13:57 -0300

    Comite inicial

---
+++ AulaES/src/Pacote1/Classe1.java
@@ -0,0 +1,16 @@
+package Pacote1;
+
+public class Classe1 {
+	public int num1;
+	public int num2;
+	
+	public Classe1(){
+		num1 = 0;
+		num2 = 0;
+	}
+	
+	public int somar(int num1, int num2){
+		int somar = num1 + num2;
+		return somar;
+	}
+}
---
+++ AulaES/src/Pacote2/Classe2.java
@@ -0,0 +1,17 @@
+package Pacote2;
+
+public class Classe2 {
+	public int num1;
+	public int num2;
+	
+	public Classe2(){
+		num1 = 0;
+		num2 = 0;
+	}
+	
+	public int nultiplicar(int num1, int num2){
+		int nultiplicar = num1 * num2;
+		return nultiplicar;
+	}
+}
+
